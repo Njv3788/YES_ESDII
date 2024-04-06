6547b897fdc55bc8dd3bd41cedb1b968f3ae0537
FarhanAli24 <fa2606@rit.edu>
2024-04-06 00:12:08 -0400

    Updated Matlab code
    Reads through the serve1.dat file and sends those coordinates to Unity

--- Matlab/Unity_MatLab_Link.m
+++ Matlab/Unity_MatLab_Link.m
@@ -9,15 +9,47 @@ fprintf(1,"Connected to server\n");
 b = 60; % baseline [mm]
 f = 6; % focal length [mm]
 ps = 0.006;
-for i = [0 -0.1 -0.2 -0.3]
-% x,y,z,yaw[z],pitch[y],roll[x]
-pose = [i,0.1,2,0,0,0];%
-disp(i)
-unityImageLeft = unityLink(client,pose);
-%gray = rgb2gray(unityImageLeft);
-%bw = im2bw(gray,0.7);
-s = regionprops(unityImageLeft, 'centroid')
-%leftCentroid = s.Centroid;
-imshow(unityImageLeft)
 
+
+serve1_postions = fopen('serve1.dat','r');
+
+
+while ~feof(serve1_postions)
+    line = fgetl(serve1_postions);
+    if isempty(line)
+        continue;
+    end
+    
+    
+    
+    coordinates = str2num(line);  % Assuming coordinates are separated by spaces
+    
+    x = coordinates(1);
+    y = coordinates(2);
+    z = coordinates(3);
+    
+    pose = [x,y,z,0,0,0];%
+    unityImageLeft = unityLink(client,pose);
+    imshow(unityImageLeft)
+    % Check if the line was parsed successfully
+    if isempty(coordinates)
+        warning('Failed to parse line: %s', line);
+        continue;
+    end
 end
+
+
+
+
+% for i = [0 -0.1 -0.2 -0.3]
+% % x,y,z,yaw[z],pitch[y],roll[x]
+% pose = [i,0.1,2,0,0,0];%
+% disp(i)
+% unityImageLeft = unityLink(client,pose);
+% %gray = rgb2gray(unityImageLeft);
+% %bw = im2bw(gray,0.7);
+% s = regionprops(unityImageLeft, 'centroid')
+% %leftCentroid = s.Centroid;
+% imshow(unityImageLeft)
+
+%end
