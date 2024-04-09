b008df0e63b6b26b78219927c2796f8235798a13
Nathaniel Valla <77633870+Njv3788@users.noreply.github.com>
2024-04-09 10:02:33 -0400

    Added test results, Is it really that simple?

--- Matlab/Algro_test/Matlab_Algrothim_test.m
+++ Matlab/Algro_test/Matlab_Algrothim_test.m
@@ -1,23 +1,26 @@
-ball   = imread("testImage_ball.jpg");
+% Load test images
+ball_1 = imread("testImage_ball_1.jpg");
+ball_0 = imread("testImage_ball_0.jpg");
 ball_n = imread("testImage_ball_n.jpg");
 
 figure
+% Convert to Grey 
+ball_0 = rgb2gray(ball_0);
+ball_1 = rgb2gray(ball_1);
+ball_n = rgb2gray(ball_n);
+
 % show the images
-% subplot(2,2,1);imshow(ball);
-% subplot(2,2,2);imshow(ball_n);
+subplot(3,3,1);imshow(ball_n);
+subplot(3,3,2);imshow(ball_0);
+subplot(3,3,3);imshow(ball_1);
 
-% Convert to Grey and Show the Image 
-ball   = rgb2gray(ball);subplot(2,2,3);imshow(ball);
-ball_n = rgb2gray(ball_n);subplot(2,2,4);imshow(ball_n);
+% find the sums
+x_n = sum(ball_n,1);y_n = sum(ball_n,2);
+x_0 = sum(ball_0,1);y_0 = sum(ball_0,2);
+x_1 = sum(ball_1,1);y_1 = sum(ball_1,2);
 
-% Sum the columns
-b_ys = sum(ball,1);b_xs = sum(ball,2);
-n_ys = sum(ball_n,1);n_xs = sum(ball_n,2);
-
-% Display sums
-subplot(3,2,1);plot(b_ys);
-subplot(3,2,2);plot(b_xs);
-subplot(3,2,3);plot(n_ys);
-subplot(3,2,4);plot(n_xs);
-subplot(3,2,5);plot(n_ys-b_ys);
-subplot(3,2,6);plot(n_xs-b_xs);
+% plot the graphs 
+subplot(3,2,3);plot(x_n-x_0);
+subplot(3,2,4);plot(y_n-y_0);
+subplot(3,2,5);plot(x_0-x_1);
+subplot(3,2,6);plot(y_0-y_1);
--- Matlab/Matlab_Algrothim_test.m
+++ Matlab/Matlab_Algrothim_test.m
@@ -1,21 +1,36 @@
-%Load test images
-ball   = imread("testImage_ball.jpg");
-ball_n = imread("testImage_ball.jpg");
+%Initialization Parameters
+server_ip   = '127.0.0.1';     %IP address of the Unity Server
+server_port = 55001;           %Server Port of the Unity Sever
 
-figure
-%show the images
-subplot(2,2,1);imshow(ball);
-subplot(2,2,2);imshow(ball_n);
+client = tcpclient(server_ip,server_port);
+fprintf(1,"Connected to server\n");
 
-%Convert to Grey 
-ball   = rgb2gray(ball);
-ball_n = rgb2gray(ball_n);
+%depth = -2;
+b = 60; % baseline [mm]
+f = 6; % focal length [mm]
+ps = 0.006;
 
-%show the images
-subplot(2,2,1);imshow(ball);
-subplot(2,2,2);imshow(ball_n);
+       
+coordinates = [0,.1,1.5];  %coordinates are separated by spaces
 
-figure
-%show the images
-subplot(2,2,3);imshow(ball);
-subplot(2,2,4);imshow(ball_n);
+x = coordinates(1);
+y = coordinates(2);
+z = coordinates(3);
+
+pose = [x,y,z,0,0,0];%
+unityImageLeft = unityLink(client,pose);
+imshow(unityImageLeft)
+% Check if the line was parsed successfully
+
+% for i = [0 -0.1 -0.2 -0.3]
+% % x,y,z,yaw[z],pitch[y],roll[x]
+% pose = [i,0.1,2,0,0,0];%
+% disp(i)
+% unityImageLeft = unityLink(client,pose);
+% %gray = rgb2gray(unityImageLeft);
+% %bw = im2bw(gray,0.7);
+% s = regionprops(unityImageLeft, 'centroid')
+% %leftCentroid = s.Centroid;
+% imshow(unityImageLeft)
+
+%end
