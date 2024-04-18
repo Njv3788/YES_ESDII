8013fba72ef33ecb466a8558167546cfa76f6cc8
Nathaniel Valla (RIT Student) <njv3788@rit.edu>
2024-04-17 21:55:50 -0400

    Fixed problems with the simlulink scipts

--- FPGA/ravvenlabs-simulink-sobel-model-884da0c/setup.m
+++ FPGA/ravvenlabs-simulink-sobel-model-884da0c/setup.m
@@ -2,9 +2,6 @@
 % Sobel Demo Setup File
 R = 752; 
 C = 480;
-coefficients = [1 0 -1; 
-                2 0 -2;
-                1 0 -1];
 testImage = zeros(480,752,3);
 testImage(100,:,:) = 200;
 testImage = uint8(testImage);
