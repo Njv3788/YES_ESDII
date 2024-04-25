b5d94ce013d04facf2de12c3837c844ff0cc0a1a
FarhanAli24 <fa2606@rit.edu>
2024-04-25 01:33:52 -0400

    View both Cameras
    can see both cameras and control all objects

--- Matlab/Unity_MatLab_Link.m
+++ Matlab/Unity_MatLab_Link.m
@@ -14,7 +14,7 @@ ps = 0.006;
 serve1_postions = fopen('serve1.dat','r');
 
 
-%Selection
+%Selection To Control Objects
 % 1 = MainCamera
 % 2 = SecondCamera
 % 3 = Ball
@@ -29,17 +29,41 @@ while ~feof(serve1_postions)
     
     
     
-    coordinates = str2num(line);  %coordinates are separated by spaces
-    
-    x = coordinates(1);
-    y = coordinates(2);
-    z = coordinates(3);
-    
+%     coordinates = str2num(line);  %coordinates are separated by spaces
+%     
+%     x = coordinates(1);
+%     y = coordinates(2);
+%     z = coordinates(3);
+%     
+%     %x,y,z,yaw[z],pitch[y],roll[x],selection
+%     pose = [x,y,z,0,0,0,selection];
+%     
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % left camera
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % z,x,y, y rot, x rot, z rot 
     %x,y,z,yaw[z],pitch[y],roll[x],selection
-    pose = [x,y,z,0,0,0,selection];
     
+    %y,z,x
+    pose = [-9.43,1.8,1.73,0,-90,-90,2];
     unityImageLeft = unityLink(client,pose);
-    imshow(unityImageLeft)
+    subplot(1,2,1)
+    imshow(unityImageLeft);
+   
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % right camera
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % z,x,y, y rot, x rot, z rot
+    
+    %y,z,x
+    pose = [-9.43,1.82,2.66,0,-90, 0,1];
+    unityImageRight = unityLink(client,pose);  
+    subplot(1,2,2)
+    imshow(unityImageRight);
+    
+    
     % Check if the line was parsed successfully
     if isempty(coordinates)
         warning('Failed to parse line: %s', line);
